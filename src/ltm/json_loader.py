6ba749d7c2f60bae2d374880437e214fd873ccb2
Matías Pavez <matias.pavez.b@gmail.com>
2018-04-22 03:33:32 -0300

    json loader as standard ROS node

--- src/ltm/json_loader.py
+++ src/ltm/json_loader.py
@@ -3,7 +3,6 @@
 __author__ = 'Matías Pavez'
 __email__ = 'matias.pavez@ing.uchile.cl'
 
-import roslib; roslib.load_manifest('ltm')
 import rospy
 from ltm.json_parser import JsonParser
 from ltm.srv import *
@@ -13,10 +12,10 @@ class LoaderNode(object):
 
     def __init__(self):
         # parameters
-        self.filename = rospy.get_param("~filename", None)
-        if self.filename is None:
+        self.source = rospy.get_param("~source", None)
+        if self.source is None:
             raise ValueError("Filename cannot be empty.")
-        rospy.loginfo("Using filename: " + self.filename)
+        rospy.loginfo("Using filename: " + self.source)
 
         # clients
         self.add_episode_client = rospy.ServiceProxy('ltm_server/add_episode', AddEpisode)
@@ -30,23 +29,19 @@ class LoaderNode(object):
             req.episode = episode
             self.add_episode_client(req)
         except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
+            print "Service call failed: %s" % e
 
     def load_json(self):
         loader = JsonParser()
-        data = loader.load_json(self.filename)
+        data = loader.load_json(self.source)
         episode = loader.json_to_episode(data)
         self.save(episode)
 
 
 def main():
-    rospy.init_node("ltm_json_loader")
-    node = LoaderNode()
-    node.load_json()
-
-
-if __name__ == '__main__':
     try:
-        main()
+        rospy.init_node("ltm_json_loader")
+        node = LoaderNode()
+        node.load_json()
     except rospy.ROSInterruptException:
         pass
